1612ceeeef210738bb6d2540baf6d4a60e7c4790
smsm8898 <smsm8898@gripcorp.co>
2025-10-06 19:15:42 +0900

    Feat: add general ds

--- recsys/data/movielens.py
+++ recsys/data/movielens.py
@@ -1,6 +1,7 @@
 import torch
+import numpy as np
 
-class MovielensDataset(torch.utils.data.Dataset):
+class MovielensSimpleDataset(torch.utils.data.Dataset):
     def __init__(self, data, sparse_feature_names):
         self.sparse_feature_names = sparse_feature_names
         self.data = data[:, :-1]
@@ -16,3 +17,21 @@ class MovielensDataset(torch.utils.data.Dataset):
         }
         labels = self.label[idx]
         return sparse_featues, labels
+    
+class MovielensFeatureDataset(torch.utils.data.Dataset):
+    def __init__(self, df, sparse_feature_names: list[str], dense_feature_names: list[str], label="rating"):
+        self.df = df.copy()
+        self.sparse_feature_names = sparse_feature_names
+        self.dense_feature_names = dense_feature_names
+        
+        self.sparse_features = {feature_name: self.df[feature_name].values.astype(np.int64) for feature_name in self.sparse_feature_names}
+        self.dense_features = self.df[dense_feature_names].values.astype(np.float32)
+        self.label = self.df[label].values.astype(np.float32)
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        sparse_features = {k: v[idx] for k, v in self.sparse_features.items()}
+        return sparse_features, self.dense_features[idx], self.label[idx]
+            
