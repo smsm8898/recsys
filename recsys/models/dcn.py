3245b1d3e2ab603a0e1b93aada8b884a24f5ba6e
smsm8898 <smsm8898@gripcorp.co>
2025-10-18 19:53:44 +0900

    Update: DCN

--- recsys/models/dcn.py
+++ recsys/models/dcn.py
@@ -1,74 +1,82 @@
 import torch
 
+
 class DeepCrossNetwork(torch.nn.Module):
     model_name = "dcn"
     def __init__(
         self,
-        num_sparse_fields: int,
-        num_sparse_features: int,
-        num_dense_features: int,
-        latent_dim: int,
-        num_cross_layers: int,
+        num_sparse_features: dict[str, int],
+        dense_features: list[str], 
+        latent_dim: int, 
         hidden_layers: list[int],
-        *args,
-        **kwargs,
+        num_cross_layers: int,
     ):
         super().__init__()
-        self.num_sparse_fields = num_sparse_fields
         self.num_sparse_features = num_sparse_features
-        self.num_dense_features = num_dense_features
+        self.sparse_feature_names = list(num_sparse_features.keys())
+        self.F = len(self.sparse_feature_names)
+        self.dense_feature_names = dense_features
+        self.num_dense_features = len(dense_features)
         self.latent_dim = latent_dim
-        self.num_cross_layers = num_cross_layers
         self.hidden_layers = hidden_layers
+        self.num_cross_layers = num_cross_layers
 
-        # Input Embedding Layer(공유)
-        self.W_sparse = torch.nn.Embedding(num_sparse_features, latent_dim)
-        self.W_dense = torch.nn.Linear(num_dense_features, latent_dim)
+        # Embedding and stacking layer
+        self.sparse_arch = torch.nn.ModuleDict({
+            name: torch.nn.Embedding(vocab_size, latent_dim)
+            for name, vocab_size in num_sparse_features.items() 
+        })
+        self.dense_arch = torch.nn.Linear(self.num_dense_features, latent_dim, bias=False)
 
         # Cross Network
-        in_features = (num_sparse_fields + 1) * latent_dim
-        self.cross_kernels = torch.nn.ParameterList([
-            torch.nn.Parameter(torch.nn.init.xavier_normal_(torch.empty(in_features, in_features)))
-            for _ in range(num_cross_layers)
-        ])
-        self.cross_bias = torch.nn.ParameterList([
+        in_features = (self.F + 1) * latent_dim
+        self.cross_network = torch.nn.ModuleDict({
+            "w" : torch.nn.ParameterList([
+                torch.nn.Parameter(torch.nn.init.xavier_normal_(torch.empty(in_features, in_features)))
+                for _ in range(num_cross_layers)
+            ]),
+            "b": torch.nn.ParameterList([
             torch.nn.Parameter(torch.zeros(in_features, 1))
             for _ in range(num_cross_layers)
-        ])
+            ])
+        })
 
-        # Deep Network: MLP(Embedding은 공유)
+        # Deep Network
         mlp = []
-        mlp_in_features = in_features
-        for mlp_out_features in hidden_layers:
-            mlp.append(torch.nn.Linear(mlp_in_features, mlp_out_features))
+        in_features = (self.F + 1) * latent_dim
+        for out_features in hidden_layers:
+            mlp.append(torch.nn.Linear(in_features, out_features, bias=False))
             mlp.append(torch.nn.ReLU())
-            mlp_in_features = mlp_out_features
-        self.mlp = torch.nn.ModuleList(mlp)
+            in_features = out_features
+        self.mlp = torch.nn.Sequential(*mlp)
 
-        # Prediction Layer
-        final_dim = in_features + hidden_layers[-1]
-        self.classifier = torch.nn.Linear(final_dim, 1)
+        # Combination output layer
+        final_dim = (self.F + 1) * latent_dim + hidden_layers[-1]
+        self.classifier = torch.nn.Linear(final_dim, 1, bias=True)
 
-    def forward(self, sparse_features: torch.LongTensor, dense_features: torch.FloatTensor) -> torch.FloatTensor:
-        # 2-1. Embedding
-        x0_sparse = self.W_sparse(sparse_features) # [batch_size, num_sparse_fields, latent_dim]
-        x0_dense = self.W_dense(dense_features) # [batch_size, latent_dim]
-        x0 = torch.cat([torch.flatten(x0_sparse, 1), x0_dense], dim=1) # [batch_size, (num_sparse_fields + 1) * latent_dim]
+    def forward(self, features: dict[str, torch.Tensor]) -> torch.FloatTensor:
+        # 1. Embedding and stacking layer
+        x0 = torch.stack([features[feature_name] for feature_name in self.dense_feature_names], dim=1)
+        x0 = [self.dense_arch(x0)]
+        for feature_name in self.sparse_feature_names:
+            x0.append(self.sparse_arch[feature_name](features[feature_name]))
+        x0 = torch.cat(x0, dim=1) # [B, (F+1) * D]
+        
 
         # 2. Cross Network
-        x_0 = x0.unsqueeze(2)  # [batch_size, (num_sparse_fields + 1) * latent_dim, 1]
+        x_0 = x0.unsqueeze(2)  # [B, (F + 1) * D, 1]
         x_l = x_0
         for layer in range(self.num_cross_layers):
-            xl_w = torch.matmul(self.cross_kernels[layer], x_l)  # [batch_size, (num_sparse_fields + 1) * latent_dim, 1]
-            x_l = x_0 * (xl_w + self.cross_bias[layer]) + x_l     # [batch_size, (num_sparse_fields + 1) * latent_dim, 1]
-        cross_out = torch.squeeze(x_l, dim=2)                # [batch_size, (num_sparse_fields + 1) * latent_dim]
+            xl_w = torch.matmul(self.cross_network["w"][layer], x_l)  # [B, (F + 1) * D, 1]
+            x_l = x_0 * (xl_w + self.cross_network["b"][layer]) + x_l
+        cross_out = torch.squeeze(x_l, dim=2)
 
         # 2-3. Deep Network
         deep_out = x0
         for m in self.mlp:
             deep_out = m(deep_out)
 
-        # 2-4. Combination Layer
+        # 2-4. Combination output layer
         concat = torch.cat([cross_out, deep_out], dim=-1)
         logits = self.classifier(concat)
         return logits
