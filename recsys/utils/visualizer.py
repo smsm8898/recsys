c6fcdb68636eac996829b24bd4714f65ef8e411d
smsm8898 <smsm8898@gripcorp.co>
2025-10-29 20:04:31 +0900

    modify: generalize visualize func

--- recsys/utils/visualizer.py
+++ recsys/utils/visualizer.py
@@ -5,13 +5,13 @@ def plot_history(history):
     metrics = list(history.keys())
     epochs = len(history[metrics[0]])
     
-    _, axes = plt.subplots(1, 3, figsize=(18, 5), sharey=False)
+    fig, ax = plt.subplots(1, 3, figsize=(18, 5), sharey=False)
     for i, metric in enumerate(metrics):
-        ax = axes[i]
-        ax.plot(range(1, epochs+1), history[metric], marker="o", label=metric)
-        ax.set_title(metric.upper())
-        ax.set_xlabel("Epochs")
-        ax.grid(True, linestyle="--", alpha=0.6)
+        
+        ax[i].plot(range(1, epochs+1), history[metric], marker="o", label=metric)
+        ax[i].set_title(metric.upper())
+        ax[i].set_xlabel("Epochs")
+        ax[i].grid(True, linestyle="--", alpha=0.6)
     
     plt.tight_layout()
     plt.show()
@@ -28,19 +28,19 @@ def plot_metrics(results):
     
     metrics, models, ks = sorted(set(metrics)), sorted(set(models)), sorted(set(ks))
 
-    _, axes = plt.subplots(1, len(metrics), figsize=(18, 5), sharey=False)
+    fig, ax = plt.subplots(1, len(metrics), figsize=(18, 5), sharey=False)
     
     for i, metric_name in enumerate(metrics):
-        ax = axes[i]
         for model_name in models:
             values = [results[f"{metric_name}_{model_name}_{k}"] for k in ks]
-            ax.plot(ks, values, marker="o", label=model_name)
+            ax[i].plot(ks, values, marker="o", label=model_name)
     
-        ax.set_title(metric_name.upper())
-        ax.set_xlabel("K")
-        ax.legend()
-        ax.grid(True, linestyle="--", alpha=0.6)
-    
-    plt.tight_layout()
+        ax[i].set_title(metric_name.upper())
+        ax[i].legend()
+        ax[i].grid(True, linestyle="--", alpha=0.6)
+        
+    fig.supxlabel("K")
+    fig.suptitle(f"{len(models)} Models Ranking Metrics")
+    fig.tight_layout()
     plt.show()
     plt.close()
